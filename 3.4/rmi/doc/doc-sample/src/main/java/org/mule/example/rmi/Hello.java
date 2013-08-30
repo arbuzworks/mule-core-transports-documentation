2a7fb9310f9b3d3f87e5536fc23e59db204ca16a
soltanidana <dana.soltani@mulesoft.com>
2013-08-30 09:51:36 -0700

    Updating samples

--- 3.4/rmi/doc/doc-sample/src/main/java/org/mule/example/rmi/Hello.java
+++
@@ -1,8 +0,0 @@
-package org.mule.example.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Hello extends Remote {
-    String sayHello(String name) throws RemoteException;
-}
